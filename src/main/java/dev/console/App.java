04120d62ec3ea8191bcf25f5688107c43a2b1436
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-23 11:13:45 +0200

    Realisation du TP Logging

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+
+/**
+ * @author eltahhansana
+ *
+ */
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implementation Logback");
+		
+		new AppService().executer("test");
+	}
+
+}
